0b6628daad901e687ceea75e9d1b7e1be6624771
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-15 03:04:58 -0800

    algos

---
+++ week1/day1/basic13/group1.js
@@ -0,0 +1,58 @@
+var members = ["Chris B", "Monica H"]
+// Create a function for each
+
+// 1. Print 1-255
+// Print all the integers from 1 to 255.
+function print1To255() {
+  // code here
+}
+
+print1To255()
+
+// 2. Print Odds 1-255
+// Print all odd integers from 1 to 255.
+// printOdds1To255()
+
+// 3. Print Ints and Sum 0-255
+// Print integers from 0 to 255, and with each integer print the sum so far.
+// printIntsAndSum0To255()
+
+// 4. Iterate and Print Array
+// Iterate through a given array, printing each value.
+// printArrayVals(arr)
+
+// 5. Find and Print Max
+// Given an array, find and print its largest element.
+// printMaxOfArray(arr)
+
+// 6. Get and Print Average
+// Analyze an array’s values and print the average.
+// printAverageOfArray(arr)
+
+// 7. Array with Odds
+// Create an array with all the odd integers between 1 and 255 (inclusive).
+// returnOddsArray1To255()
+
+// 8. Square the Values
+// Square each value in a given array, returning that same array with changed values.
+// squareArrayVals(arr)
+
+// 9. Greater than Y
+// Given an array and a value Y, count and print the number of array values greater than Y.
+// returnArrayCountGreaterThanY(arr, y)
+
+// 10. Zero Out Negative Numbers
+// Return the given array, after setting any negative values to zero.
+// zeroOutArrayNegativeVals(arr)
+
+// 11. Max, Min, Average
+// Given an array, print the max, min and average values for that array.
+// printMaxMinAverageArrayVals(arr)
+
+// 12. Shift Array Values
+// Given an array, move all values forward (to the left) by one index, dropping the first value and leaving a 0 (zero) value at the end of the array.
+// shiftArrayValsLeft(arr)
+
+// 13. Swap String For Array Negative Values
+// Given an array of numbers, replace any negative values with the string 'Dojo'.
+// swapStringForArrayNegativeVals(arr)
