74d3ec7bffb4ce7c4a7aef9d782aaed6a8c4030b
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-15 21:53:54 -0800

    algos

---
+++ week1/day2/algos.js
@@ -0,0 +1,53 @@
+/*
+  Recreate the .push method without using .push
+
+  Given an array and a new item, add the new item to the back of the array
+  return the new length of the array
+
+  What index are you adding a new item at?
+*/
+
+// const arr1 = ["a", "b", "c"]
+// const newItem1 = "d"
+// const expected1 = 4
+// const expectedArr1 = ["a", "b", "c", "d"]
+
+// const arr2 = []
+// const newItem2 = "a"
+// const expected2 = 1
+// const expectedArr2 = ["a"]
+
+function push(arr, newItem) {
+  // code here
+}
+
+/* ******************************************************************************** */
+
+/*
+  Given an array and an int which represents the position starting from the back,
+  return the nth-to-last element
+*/
+
+// const arr1 = ["a", "b", "c", "d"]
+// const idx1 = 1
+// const expected1 = "d"
+
+// const arr2 = ["a", "b", "c", "d"]
+// const idx2 = 2
+// const expected2 = "c"
+
+// const arr3 = ["a", "b", "c", "d"]
+// const idx3 = 0
+// const expected3 = null
+
+// const arr4 = ["a", "b", "c", "d"]
+// const idx4 = -1
+// const expected4 = null
+
+function nthLast(arr, nthToLast) {
+  // code here
+}
+
+/* ******************************************************************************** */
+
+// After finishing, finish the basic algos from yesterday that you did not finish.
