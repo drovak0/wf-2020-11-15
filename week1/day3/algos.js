961964a73ebc2683026d4e55aa282cbb995fe1c3
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-16 21:57:42 -0800

    algos

---
+++ week1/day3/algos.js
@@ -0,0 +1,45 @@
+/*
+  Recreate the pop method without using .pop()
+
+  Remove and return the last item from a given array
+
+  After removing an item from an array, what else changes?
+*/
+
+// const arr1 = [1, 2, 3]
+// const expected1 = 3
+// // what arr1 should be after running pop function
+// const expectedArr1 = [1, 2]
+
+function pop(arr) {
+  // code here
+}
+
+/* ******************************************************************************** */
+
+/*
+  Given an array and an item to search for,
+  return the index where the item is found,
+
+  return -1 to represent not found
+  */
+
+// const arr1 = ["a", "b", "c"]
+// const searchItem1 = "c"
+// const expected1 = 2
+
+// const arr2 = ["a", "b", "c"]
+// const searchItem2 = 5
+// const expected2 = -1
+
+// const arr3 = ["c", "a", "b", "c"]
+// const searchItem3 = "c"
+// const expected3 = 0
+
+// const arr4 = []
+// const searchItem4 = 5
+// const expected4 = -1
+
+function indexOf(arr, searchItem) {
+  // code here
+}
