29c544e8d04aa9859d0c523f08d59a80d1b03006
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-17 21:52:21 -0800

    algos

---
+++ week1/day4/algos.js
@@ -0,0 +1,48 @@
+/*
+  Given an array of integers
+  return the index where the smallest integer is located
+
+  return -1 if there is no smallest integer (array is empty)
+  since -1 is not a valid index, this indicates it couldn't be found
+
+  If the smallest integer occurs more than once, return the index of the first one.
+*/
+
+// const nums1 = [5, 2, 3]
+// const expected1 = 1
+
+// const nums2 = [5, 2, 2, 3]
+// const expected2 = 1
+
+// const nums3 = []
+// const expected3 = -1
+
+function indexOfMinVal(nums) {
+  // code here
+}
+
+/* ******************************************************************************** */
+
+/*
+  Array: Second-Largest
+
+  Return the second-largest element of an array, or null if there is none.
+
+  Bonus: do it with one loop and no nested loops
+*/
+
+// const nums1 = [2, 3, 1, 4]
+// const expected1 = 3
+
+// const nums2 = [3, 3]
+// const expected2 = null
+
+// const nums3 = [2]
+// const expected3 = null
+
+// const nums4 = []
+// const expected4 = null
+
+function secondLargest(nums) {
+  // code here
+}
