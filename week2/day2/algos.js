a2976b444a3d9b28359830a2ad7d45d0d26c51bd
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-22 22:12:13 -0800

    algos

---
+++ week2/day2/algos.js
@@ -0,0 +1,44 @@
+/*
+  Array: Concat
+
+  .push allowed: arrName.push(newItem)
+
+  Replicate JavaScript’s concat() which combines two arrays into one NEW array
+
+  Input: two arrays
+  Output: one NEW array with the items of both in the original order
+*/
+
+// const arrA1 = ["a", "b"]
+// const arrB1 = [1, 2, 3]
+// const expected1 = ["a", "b", 1, 2, 3]
+
+// const arrA2 = [1, 2, 3]
+// const arrB2 = ["a", "b"]
+// const expected2 = [1, 2, 3, "a", "b"]
+
+function concat(arr1, arr2) {
+  // code here
+}
+
+/* ******************************************************************************** */
+
+/*
+  From zoom interview with student in year 2020
+
+  Given one array,
+  return a new array that contains all of the original items duplicated twice
+*/
+
+// const arr1 = ["a", "b", "c"];
+// const expected1 = ["a", "b", "c", "a", "b", "c"];
+
+// const arr2 = ["a"];
+// const expected2 = ["a", "a"];
+
+// const arr3 = [];
+// const expected3 = [];
+
+function concatArrWithSelf(arr) {
+  // code here
+}
