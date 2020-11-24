a087ed2377f8a8a32929b598ee109d83ae9807a3
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-23 22:04:14 -0800

    algos

---
+++ week2/day3/algos.js
@@ -0,0 +1,68 @@
+/*
+  Given an array, reverse it's items in place
+  return the array after reversing it
+
+  Do it in place without using any built in methods
+*/
+
+// const arr1 = [1, 2, 3]
+// const expected1 = [3, 2, 1]
+
+// const arr2 = ["a", "b"]
+// const expected2 = ["b", "a"]
+
+// const arr3 = ["a"]
+// const expected3 = ["a"]
+
+// const arr4 = []
+// const expected4 = []
+
+function reverseArr(arr) {
+  // code here
+}
+
+/* ******************************************************************************** */
+
+/*
+  Recreate the built in .slice method
+
+  Given an array, a start index, and an end index,
+  return a NEW array that has only the elements from
+  the start index (inclusive) to the end index (exclusive)
+
+  What should you do if the provided end index is out of bounds?
+*/
+
+// const arr1 = ["a", "b", "c", "d", "e"];
+// const startIdx1 = 0;
+// const endIdx1 = 5;
+// const expected1 = ["a", "b", "c", "d", "e"];
+
+// const arr2 = ["a", "b", "c", "d", "e"];
+// const startIdx2 = 0;
+// const endIdx2 = 1;
+// const expected2 = ["a"];
+
+// const arr3 = ["a", "b", "c", "d", "e"];
+// const startIdx3 = 1;
+// const endIdx3 = 2;
+// const expected3 = ["b"];
+
+// const arr4 = ["a", "b", "c", "d", "e"];
+// const startIdx4 = -100;
+// const endIdx4 = 100;
+// const expected4 = ["a", "b", "c", "d", "e"];
+
+// const arr5 = ["a", "b", "c", "d", "e"];
+// const startIdx5 = 0;
+// const endIdx5 = 0;
+// const expected5 = [];
+
+// const arr6 = ["a", "b", "c", "d", "e"];
+// const startIdx6 = 1;
+// const endIdx6 = 1;
+// const expected6 = [];
+
+function slice(arr, startIdx, endIdx) {
+  // code here
+}
