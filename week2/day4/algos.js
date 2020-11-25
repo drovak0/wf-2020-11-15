2dbcc46249178ed4522c922014be8227bf3a163d
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-24 22:09:21 -0800

    algos

---
+++ week2/day4/algos.js
@@ -0,0 +1,51 @@
+/*
+  Given an array and an index,
+  remove the item at that index.
+  return the removed item
+
+  No built-in array methods except pop().
+*/
+
+// const arr2 = ["a", "b", "c"]
+// const removeIdx2 = 3
+// const expected2 = null
+// const arr2Expected = ["a", "b", "c"]
+
+// const arr3 = ["a", "b", "c"]
+// const removeIdx3 = -3
+// const expected3 = null
+// const arr3Expected = ["a", "b", "c"]
+
+function removeAt(arr, idx) {
+  // code here
+}
+
+const arr1 = ["a", "b", "c"]
+const removeIdx1 = 1
+
+var returnedValue = removeAt(arr1, removeIdx1)
+
+const expected1 = "b"
+console.log(returnedValue === expected1) // true
+
+console.log(arr1)
+const arr1Expected = ["a", "c"]
+
+/* ******************************************************************************** */
+
+/*
+  Given an array, move the minimum value to the front
+  return the array after done
+
+  No built in methods
+*/
+
+// const nums1 = [1, 5, 2, 9];
+// const expected1 = [1, 5, 2, 9];
+
+// const nums2 = [5, 1, 0, 2, 3, 0];
+// const expected2 = [0, 5, 1, 2, 3, 0];
+
+function minToFront(nums) {
+  // code here
+}
