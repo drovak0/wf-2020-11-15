0653f59f70bad88f9da9316589bd886316144e8c
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-28 22:10:14 -0800

    algos

---
+++ week3/day1/algos.js
@@ -0,0 +1,52 @@
+/*
+  String: Reverse
+
+  Given a string,
+  return a new string that is the given string reversed
+*/
+
+// const str1 = "creature"
+// const expected1 = "erutaerc"
+
+// const str2 = "dog"
+// const expected2 = "god"
+
+function reverseString(str) {}
+
+
+/*****************************************************************************/
+
+/*
+  Acronyms
+
+  Create a function that, given a string, returns the string’s acronym
+  (first letter of each word capitalized).
+
+  Do it with .split first if you need to, then try to do it without
+*/
+
+// const str1 = " there's no free lunch - gotta pay yer way. ";
+// const expected1 = "TNFL-GPYW";
+
+// const str2 = "Live from New York, it's Saturday Night!";
+// const expected2 = "LFNYISN";
+
+function acronymize(str) {}
+
+/*****************************************************************************/
+
+/* case insensitive string compare */
+
+// const strA1 = "ABC";
+// const strB1 = "abc";
+// const expected1 = true;
+
+// const strA2 = "ABC";
+// const strB2 = "abd";
+// const expected2 = false;
+
+// const strA3 = "ABC";
+// const strB3 = "bc";
+// const expected3 = false;
+
+function caseInsensitiveStringCompare(strA, strB) {}
