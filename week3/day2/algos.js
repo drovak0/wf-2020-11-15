2d9351a283b4d9ab36d7709473a6090639b30bef
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-29 22:06:35 -0800

    algos

---
+++ week3/day2/algos.js
@@ -0,0 +1,42 @@
+/*
+  Given an arr and a separator string, output a string of every item in the array separated by the separator.
+
+  No trailing separator at the end
+  Default the separator to a comma with a space after it if no separator is provided
+*/
+
+// const arr1 = [1, 2, 3]
+// const separator1 = ", "
+// const expected1 = "1, 2, 3"
+
+// const arr2 = [1, 2, 3]
+// const separator2 = "-"
+// const expected2 = "1-2-3"
+
+// const arr3 = [1, 2, 3]
+// const separator3 = " - "
+// const expected3 = "1 - 2 - 3"
+
+// const arr4 = [1]
+// const separator4 = ", "
+// const expected4 = "1"
+
+// const arr5 = []
+// const separator5 = ", "
+// const expected5 = ""
+
+function join(arr, separator) {}
+
+/*****************************************************************************/
+
+/*
+  Book Index
+
+  Given an arry of ints representing page numbers
+  return a string with the page numbers formatted as page ranges when the nums span a consecutive range
+*/
+
+// const nums1 = [1, 13, 14, 15, 37, 38, 70];
+// const expected1 = "1, 13-15, 37-38, 70";
+
+function bookIndex(nums) {}
