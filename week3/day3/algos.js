5fb84495b1f13f723c0cf008b32b2748ad56ea2f
Chris Bautista <CBaut@users.noreply.github.com>
2020-11-30 05:12:14 -0800

    algos

---
+++ week3/day3/algos.js
@@ -0,0 +1,36 @@
+/*
+  String Encode
+
+  You are given a string that may contain sequences of consecutive characters.
+  Create a function to shorten a string by including the character,
+  then the number of times it appears.
+
+
+  If final result is not shorter (such as "bb" => "b2" ),
+  return the original string.
+  */
+
+// const str1 = "aaaabbcddd"
+// const expected1 = "a4b2c1d3"
+
+// const str2 = ""
+// const expected2 = ""
+
+// const str3 = "a"
+// const expected3 = "a"
+
+// const str4 = "bbcc"
+// const expected4 = "bbcc"
+
+function encodeStr(str) {}
+
+/*****************************************************************************/
+
+/*
+  String Decode
+*/
+
+// const str1 = "a3b2c1d3";
+// const expected1 = "aaabbcddd";
+
+function decodeStr(str) {}
