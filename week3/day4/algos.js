940e5dc3e7185ea00c3a69c32c9fe38001bd980c
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-01 22:05:29 -0800

    algos

---
+++ week3/day4/algos.js
@@ -0,0 +1,49 @@
+/*
+  String: Is Palindrome
+
+  Create a function that returns a boolean whether the string is a strict palindrome.
+    - palindrome = string that is same forwards and backwards
+
+  Do not ignore spaces, punctuation and capitalization
+ */
+
+// const str1 = "a x a"
+// const expected1 = true
+
+// const str2 = "racecar"
+// const expected2 = true
+
+// const str3 = "Dud"
+// const expected3 = false
+
+// const str4 = "oho!"
+// const expected4 = false
+
+function isPalindrome(str) {}
+
+/*****************************************************************************/
+
+/*
+Longest Palindrome
+
+For this challenge, we will look not only at the entire string provided, but also at the substrings within it. Return the longest palindromic substring.
+
+Strings longer or shorter than complete words are OK.
+
+All the substrings of "abc" are:
+a, ab, abc, b, bc, c
+*/
+
+// const str1 = "what up, daddy-o?";
+// const expected1 = "dad";
+
+// const str2 = "uh, not much";
+// const expected2 = "u";
+
+// const str3 = "Yikes! my favorite racecar erupted!";
+// const expected3 = "e racecar e";
+
+function longestPalindromicSubstring(str) {}
+
+/*****************************************************************************/
+
