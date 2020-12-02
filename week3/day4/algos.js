8e8faf7d1f3a50de25d7851ea026cd647ed16e9d
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-01 23:29:47 -0800

    solutions

--- week3/day4/algos.js
+++ week3/day4/algos.js
@@ -19,7 +19,19 @@
 // const str4 = "oho!"
 // const expected4 = false
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+    // Looping inwards from both sides.
+    let leftChar = str[i]
+    let rightChar = str[str.length - 1 - i]
+
+    if (leftChar !== rightChar) {
+      return false // early exit
+    }
+  }
+  return true
+}
+
 
 /*****************************************************************************/
 
@@ -37,13 +49,39 @@ a, ab, abc, b, bc, c
 // const str1 = "what up, daddy-o?";
 // const expected1 = "dad";
 
-// const str2 = "uh, not much";
-// const expected2 = "u";
+const str2 = "uh, not much";
+const expected2 = "u";
 
-// const str3 = "Yikes! my favorite racecar erupted!";
-// const expected3 = "e racecar e";
+const str3 = "Yikes! my favorite racecar erupted!";
+const expected3 = "e racecar e";
 
-function longestPalindromicSubstring(str) {}
+function longestPalindromicSubstring(str) {
+  if (str.length === 0) return ''
+  // longestPal
+  var longestPal = str[0]
+  // loop to read the entire string
+  for (let i = 0; i < str.length; i++) {
+    // loop
+    for (let j = i + 1; j < str.length; j++) {
+      // create another string
+      // slice
+      var sub = str.slice(i,j)
 
+      // take a portion of the string
+      // check if isPalinDrome
+      if (isPalindrome(sub)) {
+        // if the portion.length is longer then longestPal.length
+        if (sub.length > longestPal.length) {
+          longestPal = sub
+        }
+      }
+    }
+  }
+
+  // return longestPal
+  console.log(longestPal)
+  return longestPal
+}
+
+longestPalindromicSubstring(str2)
 /*****************************************************************************/
-
