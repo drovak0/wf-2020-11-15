086e0ad4f8816ca99e809c1928c0f1cd658f2ddc
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-03 05:09:30 -0800

    algos

---
+++ week4/day1/algos.js
@@ -0,0 +1,45 @@
+// Theme: Strings & Dicts
+
+/*
+  Given an array of strings
+  return a sum to represent how many times each array item is found (a frequency table)
+
+  Useful methods:
+    Object.hasOwnProperty("keyName")
+      - returns true or false if the object has the key or not
+
+    Python: key in dict
+*/
+
+// const arr1 = ["a", "a", "a"]
+// const expected1 = {
+//   a: 3,
+// }
+
+// const arr2 = ["a", "b", "a", "c", "B", "c", "c", "d"]
+// const expected2 = {
+//   a: 2,
+//   b: 1,
+//   c: 3,
+//   B: 1,
+//   d: 1,
+// }
+
+// const arr3 = []
+// const expected3 = {}
+
+function frequencyTableBuilder(arr) {}
+
+/*****************************************************************************/
+
+/*
+  Reverse Word Order
+
+  Given a string of words (with spaces)
+  return a new string with words in reverse sequence.
+*/
+
+// const str1 = "This is a test";
+// const expected1 = "test a is This";
+
+function reverseWordOrder(wordsStr) {}
