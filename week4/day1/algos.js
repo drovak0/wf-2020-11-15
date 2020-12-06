763f9adf2bb57c3b0f0d67aee9f89e2a61cd3d34
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-05 23:17:23 -0800

    solution

--- week4/day1/algos.js
+++ week4/day1/algos.js
@@ -28,7 +28,22 @@
 // const arr3 = []
 // const expected3 = {}
 
-function frequencyTableBuilder(arr) {}
+function frequencyTableBuilder(arr) {
+  var obj = {}
+
+  for (let index = 0; index < arr.length; index++) {
+    var key = arr[index]
+    if (obj.hasOwnProperty(key)) {
+      obj[key] += 1
+    } else {
+      obj[key] = 1
+    }
+  }
+  console.log(obj)
+  return obj
+}
+
+// frequencyTableBuilder(arr2)
 
 /*****************************************************************************/
 
@@ -42,4 +57,26 @@ function frequencyTableBuilder(arr) {}
 // const str1 = "This is a test";
 // const expected1 = "test a is This";
 
-function reverseWordOrder(wordsStr) {}
+function reverseString(str) {
+  // var newString = ""
+  var newStringArr = []
+  var splitString = str.split(" ")
+
+  // split
+  // reverse
+  // join
+
+  // for (var i = splitString.length - 1; i >= 1; i--) {
+  for (var i = splitString.length - 1; i >= 0; i--) {
+    // newString += splitString[i] + " "
+    newStringArr.push(splitString[i])
+  }
+  // newString += splitString[0]
+  console.log(`newStringArr: `, newStringArr);
+
+  // return newString
+  return newStringArr.join(' ')
+
+  // return str.split(' ').reverse().join(' ')
+}
+console.log(reverseString("This is a test"))
