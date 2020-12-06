7f32968328901fd538493bb22ada3d6296c7b745
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-06 04:32:38 -0800

    algos

---
+++ week4/day2/algos.js
@@ -0,0 +1,33 @@
+/*
+  Given a string,
+  return a new string with the duplicates excluded
+
+  Bonus: Keep only the last instance of each character.
+*/
+
+const str1 = "abcABC"
+const expected1 = "abcABC"
+
+const str2 = "helloo"
+const expected2 = "helo"
+
+function stringDedupe(str) {}
+
+/*****************************************************************************/
+/*
+  Given a string containing space separated words
+  Reverse each word in the string.
+
+  If you need to, use .split to start, then try to do it without.
+*/
+
+// const str1 = "hello";
+// const expected1 = "olleh";
+
+// const str2 = "hello world";
+// const expected2 = "olleh dlrow";
+
+// const str3 = "abc def ghi";
+// const expected3 = "cba fed ihg";
+
+function reverseWords(str) {}
