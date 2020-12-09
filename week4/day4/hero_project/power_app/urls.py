0cae715742a63d04716d4f19d27af0baca9cab9f
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-09 04:17:49 -0800

    refactoring

--- week4/day4/hero_project/power_app/urls.py
+++ week4/day4/hero_project/power_app/urls.py
@@ -3,6 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('add_power_to_hero', views.add_power_to_hero),
+    # Renders views
+    path('', views.index), # all heroes
+    path('powers', views.all_powers), # all powers
+    path('heroes/<int:heroo_id>', views.show_hero),
+    path('powers/<int:power_id>', views.show_power),
+
+    # Return redirects
+    path('add_power', views.add_power),
+    path('add_hero', views.add_hero),
+    path('add_power_to_hero', views.add_power_to_hero),  # add m-2-k
 ]
--- week4/day4/hero_project/power_app/views.py
+++ week4/day4/hero_project/power_app/views.py
@@ -4,25 +4,72 @@ from .models import *
 
 # Create your views here.
 
-
+# Render views
 def index(request):
 
     context = {
-      'all_heroes': Hero.objects.all(),
-      'all_powers': Power.objects.all(),
+        'all_heroes': Hero.objects.all(),
     }
     return render(request, 'index.html', context)
+
+
+def all_powers(request):
+    context = {
+        'all_powers': Power.objects.all(),
+    }
+    return render(request, 'powers.html', context)
+
+
+def show_hero(request, heroo_id):
+    context = {
+        'hero_to_display': Hero.objects.get(id=heroo_id),
+        'all_powers': Power.objects.all()
+    }
+    return render(request, 'hero.html', context)
+
+
+def show_power(request, power_id):
+    context = {
+        'power_to_display': Power.objects.get(id=power_id),
+        'all_heroes': Hero.objects.all()
+    }
+    return render(request, 'power.html', context)
+
+
+# Redirect
+def add_power(request):
+    print(request.POST)
+
+    Power.objects.create(
+        name=request.POST['name'],
+        description=request.POST['description'],
+        image=request.POST['image']
+    )
+
+    return redirect('/powers')
+
+
+def add_hero(request):
+    print(request.POST)
+
+    Hero.objects.create(
+        name=request.POST['name'],
+        description=request.POST['description'],
+        image=request.POST['image']
+    )
+
+    return redirect('/')
 
 
 def add_power_to_hero(request):
     print(request.POST)
     # 1 get the one instance
-    power = Power.objects.get(id=request.POST['power'])
+    power = Power.objects.get(id=request.POST['power_id'])
     # 2 get the second instance
-    hero = Hero.objects.get(id=request.POST['hero'])
+    hero = Hero.objects.get(id=request.POST['hero_id'])
     # 3 add one to the other
 
     # only need 1 of theses
     # power.heroes.add(hero)
     hero.powers.add(power)
-    return redirect('/')
+    return redirect(f"/heroes/{request.POST['hero_id']}")
