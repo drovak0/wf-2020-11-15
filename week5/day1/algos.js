9b1c920c34f68c4b83d92713bcda85596b707ea7
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-10 04:49:21 -0800

    algos

---
+++ week5/day1/algos.js
@@ -0,0 +1,40 @@
+// Theme: Arrays
+
+/*
+  Balance Index
+
+  Here, a balance point is ON an index, not between indices.
+
+  Return the balance index where sums are equal on either side
+  (exclude its own value).
+
+  Return -1 if none exist.
+
+*/
+
+const nums1 = [-2, 5, 7, 0, 3];
+const expected1 = 2;
+
+const nums2 = [9, 9];
+const expected2 = -1;
+
+function balanceIndex(nums) {}
+
+/*****************************************************************************/
+
+/*
+  Balance Point
+
+  Write a function that returns whether the given
+  array has a balance point BETWEEN indices,
+  where one side’s sum is equal to the other’s.
+*/
+
+const nums1 = [1, 2, 3, 4, 10];
+const expected1 = true;
+// Explanation: between indices 3 & 4
+
+const nums2 = [1, 2, 4, 2, 1];
+const expected2 = false;
+
+function balancePoint(nums) {}
