a1e7286327034bdb74c6241dfb36ecc76f1de07d
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-12 23:17:01 -0800

    solutions

--- week5/day1/algos.js
+++ week5/day1/algos.js
@@ -18,7 +18,29 @@
 // const nums2 = [9, 9];
 // const expected2 = -1;
 
-function balanceIndex(nums) {}
+function balanceIndex(nums) {
+  if (nums.length < 3) {
+    return -1
+  }
+
+  let left = nums[0]
+  let right = 0
+
+  for (let i = 2; i < nums.length; i++) {
+    right += nums[i]
+  }
+
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (left === right) {
+      return i
+    }
+
+    right -= nums[i + 1]
+    left += nums[i]
+  }
+  return -1
+}
+console.log(balanceIndex(nums1))
 
 /*****************************************************************************/
 
@@ -37,4 +59,27 @@ function balanceIndex(nums) {}
 // const nums2 = [1, 2, 4, 2, 1];
 // const expected2 = false;
 
-function balancePoint(nums) {}
+function balancePoint(nums) {
+  const length = nums.length
+
+  if (length < 2) {
+    return false
+  }
+
+  let left = nums[0]
+  let right = 0
+
+  for (let i = 1; i < length; i++) {
+    right += nums[i]
+  }
+
+  for (let i = 1; i < length; i++) {
+    if (left === right) {
+      return true
+    }
+
+    right -= nums[i]
+    left += nums[i]
+  }
+  return false
+}
