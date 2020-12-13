8fc7e6402b7c3abf3535b4473f8699f872dc4a5a
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-12 22:08:46 -0800

    comments

--- week5/day1/algos.js
+++ week5/day1/algos.js
@@ -12,11 +12,11 @@
 
 */
 
-const nums1 = [-2, 5, 7, 0, 3];
-const expected1 = 2;
+// const nums1 = [-2, 5, 7, 0, 3];
+// const expected1 = 2;
 
-const nums2 = [9, 9];
-const expected2 = -1;
+// const nums2 = [9, 9];
+// const expected2 = -1;
 
 function balanceIndex(nums) {}
 
@@ -30,11 +30,11 @@ function balanceIndex(nums) {}
   where one side’s sum is equal to the other’s.
 */
 
-const nums1 = [1, 2, 3, 4, 10];
-const expected1 = true;
+// const nums1 = [1, 2, 3, 4, 10];
+// const expected1 = true;
 // Explanation: between indices 3 & 4
 
-const nums2 = [1, 2, 4, 2, 1];
-const expected2 = false;
+// const nums2 = [1, 2, 4, 2, 1];
+// const expected2 = false;
 
 function balancePoint(nums) {}
