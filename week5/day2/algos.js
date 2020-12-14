6dd9ec16ad32708bd2c61e53af9ec24832c6d4a7
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-13 21:51:52 -0800

    algos

---
+++ week5/day2/algos.js
@@ -0,0 +1,27 @@
+/*
+  Array: Binary Search (non recursive)
+
+  Given a sorted array and a value, return whether the array contains that value.
+  Do not sequentially iterate the array. Instead, ‘divide and conquer’,
+  taking advantage of the fact that the array is sorted.
+*/
+
+// const nums1 = [1, 3, 5, 6]
+// const searchNum1 = 4
+// const expected1 = false
+
+// const nums2 = [4, 5, 6, 8, 12]
+// const searchNum2 = 5
+// const expected2 = true
+
+// const nums3 = [3, 4, 6, 8, 12]
+// const searchNum3 = 3
+// const expected3 = true
+
+// const nums4 = [2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9]
+// const searchNum4 = 2
+// const expected3 = true
+
+function binarySearch(sortedNums, searchNum) {
+  // pseudo code
+}
