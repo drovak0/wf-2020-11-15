17bdc09118e06fafb72de78b2f315c5638c1a57e
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-15 04:11:00 -0800

    algos

---
+++ week5/day4/algos.js
@@ -0,0 +1,49 @@
+/*
+  Given an int to represent how much change is needed
+  calculate the fewest number of coins needed to create that change,
+  using the standard US denominations
+*/
+
+// const cents1 = 25
+// const expected1 = { quarter: 1 }
+
+// const cents2 = 50
+// const expected2 = { quarter: 2 }
+
+// const cents3 = 9
+// const expected3 = { nickel: 1, penny: 4 }
+
+// const cents4 = 99
+// const expected4 = { quarter: 3, dime: 2, penny: 4 }
+
+function fewestCoinChange(cents) {}
+
+/* ******************************************************************************** */
+
+/*
+  Missing Value
+
+  You are given an array of length N that contains, in no particular order,
+  integers from 0 to N . One integer value is missing.
+  Quickly determine and return the missing value.
+*/
+
+// const nums1 = [3, 0, 1];
+// const expected1 = 2;
+
+// const nums2 = [3, 0, 1, 2];
+// const expected2 = null;
+// // Explanation: nothing is missing
+
+/*
+  Bonus: now the lowest value can now be any integer (including negatives),
+  instead of always being 0.
+*/
+
+// const nums3 = [2, -4, 0, -3, -2, 1];
+// const expected3 = -1;
+
+// const nums4 = [5, 2, 7, 8, 4, 9, 3];
+// const expected4 = 6;
+
+function missingValue(nums) {}
