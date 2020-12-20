b17602e8f4531408fa7796992192c76ee55108b1
Chris Bautista <CBaut@users.noreply.github.com>
2020-12-19 21:44:10 -0800

    algos

---
+++ week6/day1/algos.js
@@ -0,0 +1,44 @@
+// Recursion
+
+// Base case(s)
+// Forward movement
+
+// return value or function call
+// Call Stack
+// Pass by value/reference
+
+/* ******************************************************************************** */
+
+/*
+  Recursively sum an arr of ints
+*/
+
+// const nums1 = [1, 2, 3]
+// const expected1 = 6
+
+// add params if needed for recursion
+
+function sumArr(nums) {}
+
+/* ******************************************************************************** */
+
+/*
+  Recursive Sigma
+
+  Input: integer
+  Output: sum of integers from 1 to Input integer
+
+*/
+
+// const num1 = 5
+// const expected1 = 15
+// // Explanation: (1+2+3+4+5)
+
+// const num2 = 2.5
+// const expected2 = 3
+// // Explanation: (1+2)
+
+// const num3 = -1
+// const expected3 = 0
+
+function recursiveSigma(n) {}
