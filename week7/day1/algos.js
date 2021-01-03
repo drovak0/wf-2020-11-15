6bf73a4e56e5dc0c89e63b120ae02e86702bdf49
Chris Bautista <CBaut@users.noreply.github.com>
2021-01-02 23:19:25 -0800

    solutions

--- week7/day1/algos.js
+++ week7/day1/algos.js
@@ -19,30 +19,48 @@
 
 // const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-function bubbleSort(nums) {}
+function bubbleSort(nums) {
+  let isSorted = false
+
+  while (isSorted === false) {
+    isSorted = true
+
+    for (let i = 0; i < nums.length - 1; i++) {
+      const j = i + 1
+
+      if (nums[i] > nums[j]) {
+        isSorted = false
+        const temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+      }
+    }
+  }
+  return unorderedNums
+}
 
 /*****************************************************************************/
 
 /*
-  Selection sort works by iterating through the list, finding the minimum
-  value, and moving it to the beginning of the list. Then, ignoring the first
-  position, which is now sorted, iterate through the list again to find the
-  next minimum value and move it to index 1. This continues until all values
-  are sorted.
+Selection sort works by iterating through the list, finding the minimum
+value, and moving it to the beginning of the list. Then, ignoring the first
+position, which is now sorted, iterate through the list again to find the
+next minimum value and move it to index 1. This continues until all values
+are sorted.
 
-  Unstable sort.
+Unstable sort.
 
-  Time Complexity
-    - Best: O(n^2) quadratic.
-    - Average: O(n^2) quadratic.
-    - Worst: O(n^2) quadratic.
+Time Complexity
+- Best: O(n^2) quadratic.
+- Average: O(n^2) quadratic.
+- Worst: O(n^2) quadratic.
 
-  Space: O(1) constant.
+Space: O(1) constant.
 
-  Selection sort is one of the slower sorts.
-  - ideal for: pagination, where page 1 displays 10 records for example,
-      you run selection sort for 10 iterations only to display the first 10
-      sorted items.
+Selection sort is one of the slower sorts.
+- ideal for: pagination, where page 1 displays 10 records for example,
+you run selection sort for 10 iterations only to display the first 10
+sorted items.
 */
 
 // const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -51,4 +69,54 @@ function bubbleSort(nums) {}
 
 // const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function selectionSort(nums) {}
+function selectionSort(nums) {
+  for (var i = 0; i < nums.length; i++) {
+    var min = i
+    for (var j = i; j < nums.length; j++) {
+      if (nums[j] < nums[min]) {
+        min = j
+      }
+    }
+    if (nums[i] > nums[min]) {
+      var temp = nums[i]
+      nums[i] = nums[min]
+      nums[min] = temp
+    }
+  }
+  return nums
+}
+console.log(selectionSort(numsRandomOrder1))
+console.log(selectionSort(numsReversed))
+
+
+// function selectionSort(nums) {
+  //   const len = nums.length
+  //   let selectedIdx = 0
+  //   let idxOfCurrMin = 0
+
+  //   while (selectedIdx < len) {
+    //     for (let i = selectedIdx; i < len; i++) {
+      //       if (nums[i] < nums[idxOfCurrMin]) {
+        //         idxOfCurrMin = i
+        //       }
+        //     }
+
+        //     if (nums[selectedIdx] !== nums[idxOfCurrMin]) {
+          //       const temp = nums[selectedIdx]
+          //       nums[selectedIdx] = nums[idxOfCurrMin]
+          //       nums[idxOfCurrMin] = temp
+          //     }
+          //     selectedIdx += 1
+          //     // reset idxOfCurrMin to the next selected index we are going to work with to find the next min
+          //     idxOfCurrMin = selectedIdx
+          //   }
+          //   return nums
+          // }
+
+/*****************************************************************************/
+
+// Resources
+
+// https://visualgo.net/en/sorting
+// https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
+// https://www.bigocheatsheet.com/
