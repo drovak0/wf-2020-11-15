5106e0069676c03ae2a4e1ad7b019cd74b75c44a
Chris Bautista <CBaut@users.noreply.github.com>
2021-01-03 22:07:29 -0800

    fixes

--- week7/day2/algos.js
+++ week7/day2/algos.js
@@ -39,7 +39,3 @@ const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 function insertionSort(nums) {}
 
-
-/*****************************************************************************/
-
-function insertionSort(nums) {}
