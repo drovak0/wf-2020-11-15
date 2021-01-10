22043f4f3c3dbf411dd62b5e3601752c54526784
Chris Bautista <CBaut@users.noreply.github.com>
2021-01-09 22:58:25 -0700

    algos

---
+++ week8/day1/algos.js
@@ -0,0 +1,18 @@
+/*
+  Efficiently combine two already sorted multiset arrays
+  into an array containing the sorted set intersection of the two.
+
+  Output: only the shared values between the two arrays (deduped).
+
+  Venn Diagram Visualization (bottom) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+*/
+
+const numsA1 = [0, 1, 2, 2, 2, 7]
+const numsB1 = [2, 2, 6, 6, 7]
+const expected1 = [2, 7]
+
+const numsA2 = [0, 1, 2, 2, 2, 7]
+const numsB2 = [2, 2]
+const expected2 = [2]
+
+function orderedIntersection(sortedA, sortedB) {}
