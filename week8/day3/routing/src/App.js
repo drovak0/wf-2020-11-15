f260adec8ad24f37ec8de220dc99bd147eeb60c6
Chris Bautista <CBaut@users.noreply.github.com>
2021-01-12 00:42:36 -0800

    isActive

--- week8/day3/routing/src/App.js
+++ week8/day3/routing/src/App.js
@@ -1,15 +1,39 @@
-import { Link, Router } from "@reach/router";
-// import './App.css';
+import { Link, Router } from "@reach/router"
+import { useEffect, useState } from 'react'
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-    <nav>
-      <Link to="/">Home</Link> | <Link to="dashboard">Dashboard</Link>
-    </nav>
-  </div>
-)
 
+const Home = () => {
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    const int = setInterval(
+      () => {
+        console.log(new Date().toLocaleString())
+        setIsActive(!isActive)
+        console.log(`isActive: `, isActive);
+      },
+      1000
+    )
+
+    // change class name or something like that
+    // add a class
+
+    // remove a class
+
+    return function clearInt() {
+      clearInterval(int)
+    }
+  })
+
+  return (
+    <div>
+      <h1 className={isActive ? 'active' : ''}>Home</h1>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="dashboard">Dashboard</Link>
+      </nav>
+    </div>
+  )
+}
 const Dash = () => (
   <div>
     <h1>Dash</h1>
@@ -33,4 +57,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
