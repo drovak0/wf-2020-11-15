efb898e9a0fff2bc7f7b333e4ba31b13b8d826d9
Chris Bautista <CBaut@users.noreply.github.com>
2021-01-26 23:09:32 -0800

    algos

---
+++ week10/day4/algos.js
@@ -0,0 +1,45 @@
+const hour1 = 5
+const min1 = 00
+const expected1 = "five o' clock"
+
+const hour2 = 5
+const min2 = 01
+const expected2 = "one minute past five"
+
+const hour3 = 5
+const min3 = 10
+const expected3 = "ten minutes past five"
+
+const hour4 = 5
+const min4 = 15
+const expected4 = "quarter past five"
+
+const hour5 = 5
+const min5 = 30
+const expected5 = "half past five"
+
+const hour6 = 5
+const min6 = 40
+const expected6 = "twenty minutes to six"
+
+const hour7 = 5
+const min7 = 45
+const expected7 = "quarter to six"
+
+const hour8 = 5
+const min8 = 47
+const expected8 = "thirteen minutes to six"
+
+const hour9 = 5
+const min9 = 28
+const expected9 = "twenty eight minutes past five"
+
+const hour10 = 12
+const min10 = 45
+const expected10 = "quarter to one"
+
+const hour11 = 12
+const min11 = 00
+const expected11 = "twelve o' clock"
+
+function timeInWords(hour, min) {}
