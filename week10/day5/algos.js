e351aabb99095be9b2d5891d606d1b39c43820e6
Chris Bautista <CBaut@users.noreply.github.com>
2021-01-28 02:53:14 -0800

    solution

--- week10/day5/algos.js
+++ week10/day5/algos.js
@@ -71,4 +71,36 @@ Output: []
 const ailments3 = ["existential dread"];
 
 // Time: O(n*m) where n is meds length and m is longest treatableSymptoms length
-function getMeMyMeds(ailments, meds) {}
+function getMeMyMeds(ailments, meds) {
+  let maxSymptomMatchCount = 0
+
+  // helps avoid having to loop over ailments every time
+  const ailmentsMap = {}
+  let matchedMeds = []
+
+  for (const ailment of ailments) {
+    // the value isn't important
+    ailmentsMap[ailment] = true
+  }
+
+  for (const med of meds) {
+    let symptomMatchCount = 0
+
+    for (const symptom of med.treatableSymptoms) {
+      if (ailmentsMap.hasOwnProperty(symptom)) {
+        symptomMatchCount++
+      }
+    }
+
+    if (symptomMatchCount > 0) {
+      if (symptomMatchCount === maxSymptomMatchCount) {
+        matchedMeds.push(med.name)
+      } else if (symptomMatchCount > maxSymptomMatchCount) {
+        maxSymptomMatchCount = symptomMatchCount
+        // new max found, old matchedMeds need to be replaced with new arr of the new best med
+        matchedMeds = [med.name]
+      }
+    }
+  }
+  return matchedMeds
+}
