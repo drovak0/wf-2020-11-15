04bb75a18f5a74f86297e50bc1779d5b2f04289a
Chris Bautista <CBaut@users.noreply.github.com>
2021-01-31 00:42:47 -0800

    intro

---
+++ week11/day1/java_intro/Casting.java
@@ -0,0 +1,14 @@
+class Casting {
+  public static void main(String[] args) {
+    double d = 35.25;
+    double dd = 35.99;
+
+    // casting the double d into a int
+    int i = (int) d;
+
+    // casting the double dd into a int
+    int ii = (int) dd;
+    System.out.println(i);
+    System.out.println(ii);
+  }
+}
---
+++ week11/day1/java_intro/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    // return 1;
+    System.err.println("error");
+    System.out.println("out!");
+    System.out.println("hi");
+  }
+}
