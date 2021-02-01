bda77c01d9ffd4854d928ef3bfb317ac05d4027e
Chris Bautista <CBaut@users.noreply.github.com>
2021-01-31 22:09:30 -0800

    algos

---
+++ week11/day2/algos.js
@@ -0,0 +1,29 @@
+// [ ] Create the Node class
+// [ ] Create the List class
+// [ ] Implement these functions
+
+/**
+ * Creates a new node with the given data and inserts that node at the front
+ * of this list.
+ * @param {any} data The data for the new node.
+ * @return {SinglyLinkedList} This list.
+ */
+// 0
+// ->(1)->(2)->(3)->
+// ->(0)->(1)->(2)->(3)->
+insertAtFront(data) {}
+
+/**
+ * Removes the first node of this list.
+ * @return {any} The data from the removed node.
+ */
+removeHead() {}
+// ->(0)->(1)->(2)->(3)->
+// ->(1)->(2)->(3)->
+
+// EXTRA
+/**
+ * Calculates the average of this list.
+ * @return {number|NaN} The average of the node's data.
+ */
+average() {}
