15c38b8f9c1b3ac3973f2fcda82dbc83a56752c8
Monica Hong <momo.star@gmail.com>
2021-02-01 05:27:52 -0800

    vscode project shortcuts

---
+++ week11/day2/afternoon/ProjectDemo/src/App.java
@@ -0,0 +1,12 @@
+import com.codingdojo.projectdemo.AnotherClass;
+import com.codingdojo.projectdemo.NewClass;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        NewClass nc = new NewClass();
+        System.out.println(nc);
+        AnotherClass ac = new AnotherClass();
+        System.out.println(ac);
+    }
+}
---
+++ week11/day2/afternoon/ProjectDemo/src/com/codingdojo/projectdemo/AnotherClass.java
@@ -0,0 +1,23 @@
+package com.codingdojo.projectdemo;
+
+import java.util.ArrayList;
+
+public class AnotherClass {
+    private ArrayList<String> list;
+
+    public AnotherClass() {
+    }
+
+    public AnotherClass(ArrayList<String> list) {
+        this.list = list;
+    }
+
+    public ArrayList<String> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<String> list) {
+        this.list = list;
+    }
+
+}
