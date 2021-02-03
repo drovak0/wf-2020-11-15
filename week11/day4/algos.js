dccc3edf7fd7cc82174452dd6891b2dcb25b2347
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-02 22:01:54 -0800

    algos

---
+++ week11/day4/algos.js
@@ -0,0 +1,28 @@
+//    👇
+//    👇
+// ->(1)->(2)->(3)->(4)->
+
+/**
+ * Retrieves the data of the second to last node in this list.
+ * @return {any} The data of the second to last node or null if there is no
+ *    second to last node.
+ */
+secondToLast() {
+   if (!this.head || !this.head.next) {}
+}
+/**
+ * Removes the node that has the matching given val as it's data.
+ * @param {any} val The value to compare to the node's data to find the
+ *    node to be removed.
+ * @return {boolean} Indicates if a node was removed or not.
+ */
+removeVal(val) {}
+
+// EXTRA
+/**
+ * Inserts a new node before a node that has the given value as its data.
+ * @param {any} newVal The value to use for the new node that is being added.
+ * @param {any} targetVal The value to use to find the node that the newVal
+ *    should be inserted in front of.
+ */
+prepend(newVal, targetVal) {}
