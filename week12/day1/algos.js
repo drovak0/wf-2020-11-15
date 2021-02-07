fef211193e7b796daff4517b63cc7abe13bdfc98
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-06 23:01:09 -0800

    solutions

--- week12/day1/algos.js
+++ week12/day1/algos.js
@@ -16,16 +16,48 @@ class Stack {
     console.log("world")
   }
 
-  // - [ ] push (adds new item and returns new size)
-  // - [ ] pop (returns removed item)
-  // - [ ] isEmpty
-  // - [ ] size
-  // - [ ] peek (return top item without removing)
+  // - [x] push (adds new item and returns new size)
+  push(x) {
+    this.items[this.items.length] = x
+    // this.items.push(x)
+    console.log('push: ', this.items.length)
+    return this.size();
+  }
+  // - [x] pop (returns removed item)
+  pop() {
+    if (this.isEmpty() === false) {
+      var last= this.items[this.items.length-1]
+      console.log(last);
+      this.items.length= this.items.length-1;
+      // console.log(this.items);
+      console.log("pop: ", last)
+      return last;
+      // return this.items.pop()
+    }
+    return undefined
+  }
+  // - [x] isEmpty
+  isEmpty() {
+    console.log("isEmpty: ", this.items.length === 0 ? true : false)
+    return this.items.length === 0 ? true : false;
+  }
+  // - [x] size
+  size() {
+    console.log(`size: `, this.items.length);
+
+    return this.items.length
+  }
+  // - [x] peek (return top item without removing)
+  peek() {
+    console.log(`peek: `, this.items[this.size() - 1]);
+    return this.items[this.size() - 1]
+  }
 }
 
 const testStack = new Stack()
 testStack.hello()
-testStack.push()
+testStack.push(10)
+testStack.push(20)
 testStack.pop()
 testStack.isEmpty()
 testStack.size()
