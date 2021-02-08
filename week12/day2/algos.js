56ef57670fb2d1110ee945c4b298b054e3fbaf22
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-07 22:13:55 -0800

    algos

---
+++ week12/day2/algos.js
@@ -0,0 +1,39 @@
+// - A Queue is a FIFO (First in First Out) data structure
+// - Design a class to represent a queue using an array to store the items.
+// - Recreate the queue class using a singly linked list to store the items.
+// - Create these methods for each classes:
+//   - [ ] enqueue (add item, return new size)
+//   - [ ] dequeue (remove and return item)
+//   - [ ] isEmpty
+//   - [ ] size
+//   - [ ] front (return first item without removing)
+
+
+class Queue {
+  constructor() {
+    // 0 is the front
+    // arr.length-1 is the back
+    this.items = []
+  }
+
+  enqueue () {
+
+  }
+
+  dequeue () {
+
+  }
+
+  isEmpty () {
+
+  }
+
+  size () {
+
+  }
+
+  front () {
+
+  }
+
+}
