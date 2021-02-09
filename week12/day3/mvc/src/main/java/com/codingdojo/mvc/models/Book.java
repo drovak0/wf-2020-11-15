7630dcc5b8de99f22fa39e558ad4f97175675d43
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-09 03:16:36 -0800

    fixed empty objects being returned

--- week12/day3/mvc/src/main/java/com/codingdojo/mvc/models/Book.java
+++ week12/day3/mvc/src/main/java/com/codingdojo/mvc/models/Book.java
@@ -59,4 +59,46 @@ public class Book {
     protected void onUpdate(){
       this.updatedAt = new Date();
     }
+
+    public Long getId() {
+      return id;
+    }
+
+    public void setId(Long id) {
+      this.id = id;
+    }
+
+    public String getTitle() {
+      return title;
+    }
+
+    public void setTitle(String title) {
+      this.title = title;
+    }
+
+    public String getDescription() {
+      return description;
+    }
+
+    public void setDescription(String description) {
+      this.description = description;
+    }
+
+    public String getLanguage() {
+      return language;
+    }
+
+    public void setLanguage(String language) {
+      this.language = language;
+    }
+
+    public Integer getNumberOfPages() {
+      return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+      this.numberOfPages = numberOfPages;
+    }
+
+
 }
