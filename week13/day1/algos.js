7106ba1661c7e8874d0c82faecc94f3513ce3772
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-13 23:24:03 -0800

    solutions

--- week13/day1/algos.js
+++ week13/day1/algos.js
@@ -34,7 +34,12 @@ class BinarySearchTree {
    * Determines if this tree is empty.
    * @return {boolean} Indicates if this tree is empty.
    */
-  isEmpty() {}
+  isEmpty() {
+    if (this.root === null) {
+      return true
+    }
+    return false
+  }
 
   /**
    * Inserts a new node with the given newVal in the right place to preserve
@@ -42,7 +47,43 @@ class BinarySearchTree {
    * @param {number} newVal The data to be added to a new node.
    * @return {BinarySearchTree} This tree.
    */
-  insert(newVal) {}
+  insert(newVal) {
+    // check if empty
+    if (this.isEmpty()) {
+      // if empty return tree
+      this.root = new Node(newVal)
+      return this
+    }
+    // else
+    // traverse through tree
+    // track a current
+    let current = this.root
+    // while loop
+    while (current !== null) {
+      // check if current value is less or greater
+      if (newVal < current.data) {
+        // move left or right through tree
+        if (current.left === null) {
+          current.left = new Node(newVal)
+          return this
+        } else {
+          current = current.left
+        }
+      } else if (newVal > current.data) {
+        if (current.right === null) {
+          current.right = new Node(newVal)
+          return this
+        } else {
+          current = current.right
+        }
+      } else {
+        console.log("must be equal")
+      }
+    }
+    return this
+    // create new node and add to tree
+    // return tree
+  }
 
   /**
    * Inserts a new node with the given newVal in the right place to preserver
@@ -52,9 +93,38 @@ class BinarySearchTree {
    *    the tree is being traversed.
    * @return {BinarySearchTree} This tree.
    */
-  insertRecursive(newVal, curr = this.root) {}
+  insertRecursive(newVal, current = this.root) {
+    if (this.root === null) {
+      // if empty return tree
+      this.root = new Node(newVal)
+      return this
+    }
+    // check if current value is less or greater
+    if (newVal < current.data) {
+      // move left or right through tree
+      if (current.left === null) {
+        current.left = new Node(newVal)
+        return this
+      } else {
+        // current = current.left
+        return this.insertRecursive(newVal, current.left)
+      }
+    } else if (newVal > current.data) {
+      if (current.right === null) {
+        current.right = new Node(newVal)
+        return this
+      } else {
+        // current = current.right
+        return this.insertRecursive(newVal, current.right)
+      }
+    } else {
+      console.log("must be equal")
+    }
+    return this
+  }
 }
 
+// 1
 /*
                     root
                 <-- 25 -->
@@ -67,5 +137,61 @@ class BinarySearchTree {
 */
 
 // [ ] - create a tree
+const bst = new BinarySearchTree()
 // [ ] - test insert()
+console.log(bst.isEmpty())
+bst.insertRecursive(6)
+bst.insertRecursive(5)
+bst.insertRecursive(8)
+bst.insertRecursive(3)
+bst.insertRecursive(10)
+bst.insertRecursive(1)
+// bst.insert(6)
+// bst.insert(5)
+// bst.insert(8)
+// bst.insert(3)
+// bst.insert(10)
+// bst.insert(1)
+console.log(JSON.stringify(bst))
 // [ ] - test isEmpty()
+console.log(bst.isEmpty())
+
+let ex = {
+  root: {
+    data: 6,
+    left: {
+      data: 5,
+      left: {
+        data: 3,
+        left: { data: 1, left: null, right: null },
+        right: null,
+      },
+      right: null,
+    },
+    right: {
+      data: 8,
+      left: null,
+      right: { data: 10, left: null, right: null },
+    },
+  },
+}
+
+// let ex = {
+//   root: {
+//     data: 6,
+//     left: {
+//       data: 5,
+//       left: {
+//         data: 3,
+//         left: { data: 1, left: null, right: null },
+//         right: null,
+//       },
+//       right: null,
+//     },
+//     right: {
+//       data: 8,
+//       left: null,
+//       right: { data: 10, left: null, right: null },
+//     },
+//   },
+// }
