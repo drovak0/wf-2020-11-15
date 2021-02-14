5c1339d3877ac40b59f160187e27b18eb4a3c9b7
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-13 22:11:19 -0800

    algos

---
+++ week13/day1/algos.js
@@ -0,0 +1,56 @@
+class Node {
+  /**
+   * Constructs a new instance of a BST node.
+   * @param {number} data The integer to store in the node.
+   */
+  constructor(data) {
+    this.data = data
+    /**
+     * These properties are how this node is connected to other nodes to form
+     * the tree. Similar to .next in a SinglyLinkedList except a BST node can
+     * be connected to two other nodes. To start, new nodes will not be
+     * connected to any other nodes, these properties will be set after
+     * the new node is instantiated.
+     */
+    this.left = null
+    this.right = null
+  }
+}
+
+/**
+ * Represents an ordered tree of nodes where the data of left nodes are <= to
+ * their parent and the data of nodes to the right are > their parent's data.
+ */
+class BinarySearchTree {
+  constructor() {
+    /**
+     * Just like the head of a linked list, this is the start of our tree which
+     * branches downward from here.
+     */
+    this.root = null
+  }
+
+  /**
+   * Determines if this tree is empty.
+   * @return {boolean} Indicates if this tree is empty.
+   */
+  isEmpty() {}
+
+  /**
+   * Inserts a new node with the given newVal in the right place to preserver
+   * the order of this tree.
+   * @param {number} newVal The data to be added to a new node.
+   * @return {BinarySearchTree} This tree.
+   */
+  insert(newVal) {}
+
+  /**
+   * Inserts a new node with the given newVal in the right place to preserver
+   * the order of this tree.
+   * @param {number} newVal The data to be added to a new node.
+   * @param {Node} curr The node that is currently accessed from the tree as
+   *    the tree is being traversed.
+   * @return {BinarySearchTree} This tree.
+   */
+  insertRecursive(newVal, curr = this.root) {}
+}
