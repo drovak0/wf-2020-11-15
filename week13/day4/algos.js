8f4e425c0047e06d02f6e1b3ae136915431f61d5
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-16 01:39:56 -0800

    algos

---
+++ week13/day4/algos.js
@@ -0,0 +1,28 @@
+/**
+ * Recursively counts the total number of nodes in this tree.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Node} node The current node during the traversal of this tree.
+ * @return {number} The total number of nodes.
+ */
+size(node = this.root) {}
+
+/**
+ * Calculates the height of the tree which is based on how many nodes from
+ * top to bottom (whichever side is taller).
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Node} node The current node during traversal of this tree.
+ * @return {number} The height of the tree.
+ */
+height(node = this.root) {}
+
+/**
+ * Determines if this tree is a full tree. A full tree is a tree where every
+ * node has both a left and a right except for the leaf nodes (last nodes)
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Node} node The current node during traversal of this tree.
+ * @return {boolean} Indicates if this tree is full.
+ */
+isFull(node = this.root) {}
