61d6359cf00e7563c7ab70a41b3a0eac8002f61b
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-22 21:33:09 -0800

    algos

---
+++ week14/day3/algos.js
@@ -0,0 +1,23 @@
+// DoublyLinkedList
+
+/**
+ * Inserts a new node with the given newVal after the node that has the
+ * given targetVal as it's data.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {any} targetVal The node data to find.
+ * @param {any} newVal Data for the new node.
+ * @return {boolean} Indicates if the new node was added.
+ */
+insertAfter(targetVal, newVal) {}
+
+/**
+ * Inserts a new node with the given newVal before the node that has the
+ * given targetVal as it's data.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {any} targetVal The node data to find.
+ * @param {any} newVal Data for the new node.
+ * @return {boolean} Indicates if the new node was added.
+ */
+insertBefore(targetVal, newVal) {}
